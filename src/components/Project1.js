65b0021cc1a9d13bb1503fb7576283a4472f0db5
PizzaPapst <maik.bartels2@gmail.com>
2024-09-19 22:02:26 +0200

    change to img at small screen

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -1,10 +1,33 @@
-import React from "react"
 import '../styles/project1.css';
 import '../styles/textStyles.css';
 import device from '../images/device-project1.png';
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import bachelorarbeit from '../images/bachelorarbeit.jpg';
+
 
 export default function Project() {
+    const [isScreenWide, setIsScreenWide] = useState(window.innerWidth > 1000);
+
+    useEffect(() => {
+        const handleResize = () => {
+          const isWide = window.innerWidth > 1000;
+          setIsScreenWide(isWide);
+          // Wenn der Bildschirm schmaler als 800px ist, kombiniere beide Hälften zu einem
+        };
+    
+        // Event-Listener hinzufügen
+        window.addEventListener('resize', handleResize);
+    
+        // Bei der ersten Ausführung die Bildschirmgröße prüfen
+        handleResize();
+    
+        // Cleanup: Event-Listener entfernen, wenn die Komponente unmountet
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+      
     return (
         <div className="Project">
             <div className="Project-Info">
@@ -18,13 +41,19 @@ export default function Project() {
             <div className="DividerContainer">
                 <div className="DividerProject"></div>
             </div>
+            {isScreenWide ? 
             <Link to="/bachelorarbeit" className="ImgContainer">
-                <img alt="Device" className="Device" src={device}/>
-                <div className="ImgTitleContainer">
-                    <p className="ImgTitle">Pausenhelfer</p>
-                    <p className="SubTitle">Bringt Bewegung in deinen Arbeitsalltag</p>
-                </div>
+                    <img alt="Device" className="Device" src={device}/>
+                    <div className="ImgTitleContainer">
+                        <p className="ImgTitle">Pausenhelfer</p>
+                        <p className="SubTitle">Bringt Bewegung in deinen Arbeitsalltag</p>
+                    </div> 
+            </Link> : 
+            <Link to="/bachelorarbeit" className="img">
+                <img alt="project1thumbnail" className="" src={bachelorarbeit} style={{width: "100%"}}/>
             </Link>
+            }
+            
         </div>
     )
 }
--- src/components/Project2.js
+++ src/components/Project2.js
@@ -1,11 +1,34 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
+
 import '../styles/project2.css'
 import { Link } from "react-router-dom";
 import variableImg from '../images/VariableImg.png';
 import cursor from '../images/Cursor.png';
 import variableIcon from '../images/VariableIcon.svg';
+import ds from '../images/designsystem.jpg';
+
 
 export default function Project() {
+    const [isScreenWide, setIsScreenWide] = useState(window.innerWidth > 1000);
+    useEffect(() => {
+        const handleResize = () => {
+          const isWide = window.innerWidth > 1000;
+          setIsScreenWide(isWide);
+          // Wenn der Bildschirm schmaler als 800px ist, kombiniere beide Hälften zu einem
+        };
+    
+        // Event-Listener hinzufügen
+        window.addEventListener('resize', handleResize);
+    
+        // Bei der ersten Ausführung die Bildschirmgröße prüfen
+        handleResize();
+    
+        // Cleanup: Event-Listener entfernen, wenn die Komponente unmountet
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
     return (
         <div className="Project">
             <div className="Project-Info">
@@ -19,16 +42,23 @@ export default function Project() {
             <div className="DividerContainer">
                 <div className="p2-DividerProject"></div>
             </div>
+            {isScreenWide ? 
             <Link to="/designsystem" className="p2-ImgContainer">
-                <img alt="Device" className="variableImg" src={variableImg}/>
-                <img alt="Device" className="cursor" src={cursor}/>
-                <div className="p2-ImgTitleContainer">
-                    <p className="ImgTitle">Portfolio <br/>
-                    Design System</p>
-                    <p className="SubTitle">Inklusive Variablen und Tokens</p>
-                    <img className="variableIcon" alt="icon" src={variableIcon}/>
-                </div>
+                    
+                        <img alt="Device" className="variableImg" src={variableImg}/>
+                        <img alt="Device" className="cursor" src={cursor}/>
+                        <div className="p2-ImgTitleContainer">
+                         <p className="ImgTitle">Portfolio <br/>
+                        Design System</p>
+                        <p className="SubTitle">Inklusive Variablen und Tokens</p>
+                        <img className="variableIcon" alt="icon" src={variableIcon}/>
+                    </div> 
+            </Link> : 
+            <Link to="/bachelorarbeit" className="img">
+                <img alt="project1thumbnail" className="" src={ds} style={{width: "100%"}}/>
             </Link>
+            }
         </div>
     )
 }
+
